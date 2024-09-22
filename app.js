9b497cccd08b47130f51b741087110cc3607549e
sahan7492 <sahanahmednaim92@gmail.com>
2024-09-22 23:29:37 +0600

    first java

---
+++ app.js
@@ -0,0 +1,44 @@
+// console.log("Sahan")
+// console.log(90)
+
+// Data type.....
+// 1. number : 80
+// 2. string : "name"
+// 3. boolean : true, fulse
+// 4. undefined : when variable is not defined
+// 5. null : nai
+
+var sahan = 80 ;
+// console.log(sahan)
+
+var Name = "Sahan"
+// console.log(Name)
+
+var goodBoy = true
+// console.log(goodBoy)
+
+var numan = 90 ;
+// console.log(numan)
+
+var nadiya = true
+// console.log(nadiya)
+
+var okey = null ;
+console.log(okey)
+
+// variable decleration....
+// 1. underscore : full_name
+// 2. camel case : fullName
+// 3. pescel case : FullName
+
+var full_name = "sahan ahmed naim"
+// console.log(full_name)
+
+var fullName = "Taniya TAsnim Mim"
+// console.log(fullName)
+
+var FullName = "Farafi Alamin"
+// console.log(FullName)
+
+
+
---
+++ mathmatrics.js
@@ -0,0 +1,19 @@
+var khata = 20 ;
+var kholom = 10 ;
+var total = khata + kholom ;
+// console.log(total)
+
+var amrGecheAche = 200 ;
+var bonduReDilam = 80 ;
+var amrGecheRoise = amrGecheAche - bonduReDilam ;
+// console.log(amrGecheRoise)
+
+var khataNItam = 7 ;
+var PerPice = 14 ;
+var sorbo_mut = khataNItam * PerPice
+// console.log(sorbo_mut)
+
+var TkAche = 3000 ;
+var pent_per_pice = 600 ;
+var kintamParmu = TkAche / pent_per_pice ;
+console.log(kintamParmu)
